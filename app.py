82eff67c0043c6c3c2f42a2fc6714723766d57ef
maru-skb <t125001@ed.sus.ac.jp>
2026-01-26 11:22:03 +0900

    追加

--- app.py
+++ app.py
@@ -9,18 +9,31 @@ st.title('人口推移')
 df = pd.read_csv('c01.csv')
 
 with st.sidebar:
-    selected_cats = st.multiselect('都道府県を選択してください（複数選択可）',
-                            df['都道府県名'])
-    media = st.selectbox('広告媒体を選択してください',
-                         df['media'].unique())
+    selected_pre = st.multiselect('都道府県を選択してください（複数選択可）',
+                            df['都道府県名'].unique())
+    year = st.selectbox('西暦を選択してください',
+                         df['西暦（年）'].unique())
     color = st.selectbox('分類を選択してください',
-        ['性別', '年齢層', '季節']
+        ['人口（総数）', '人口（男）', '人口（女）']
     )
-    if color == '性別':
-        color = 'sex'
-    elif color == '年齢層':
-        color = 'age'
-    elif color == '季節':
-        color = 'season'
+
     option = st.radio('表示形式を選択してください',
                       ['表', 'グラフ'])
+    
+df = df[df['都道府県名'].isin(selected_pre)]
+df = df[df['西暦（年）']== year]
+
+if option == '表':
+    st.dataframe(df, width=800, height=220)
+
+elif option == 'グラフ':
+    fig = px.scatter(
+        df,
+        x='都道府県名',
+        y='西暦（年）',
+        color=color,
+        trendline='ols',
+        range_x=[0, df['都道府県名'].max()],
+        range_y=[0, df['西暦（年）'].max()]
+    )
+    st.plotly_chart(fig)
