4fd94cea1f748e99bbcd64b83a7e621cf04a9bda
maru-skb <t125001@ed.sus.ac.jp>
2026-01-27 01:13:42 +0900

    追加

--- app.py
+++ app.py
@@ -8,11 +8,8 @@ st.title('人口推移')
 
 df = pd.read_csv('c01.csv')
 
-st.title('アプリの概要')
-st.write('本アプリは国勢調査の人口データを用いて、都道府県別の人口推移を可視化するWebアプリです。')
-
 with st.sidebar:
-    st.header('設定')
+    st.header('メニュー')
 
     # 都道府県を選択（複数可）
     selected_pre = st.multiselect('都道府県を選択してください（複数選択可）',
@@ -26,25 +23,55 @@ with st.sidebar:
 
     # 表示形式を選択
     option = st.radio('表示形式を選択してください',
-                      ['表', 'グラフ'])
+                      ['アプリの概要', '表', 'グラフ'])
     
 # 年順に並べ替える
-df = df[df['都道府県名'].isin(selected_pre).sort_values('西暦（年）')]
+df = df[df['都道府県名'].isin(selected_pre)]
+df = df.sort_values('西暦（年）')
 
-if option == '表':
-    st.subheader('データ一覧')
-    st.dataframe(df[['都道府県名', '西暦（年）', population_type]])
+if option == 'アプリの概要':
+    st.title('アプリの概要')
+    st.write('本アプリは、e-statの「国勢調査」のデータを使用して、都道府県別の人口推移を可視化します。')
+    st.write('サイドバーから都道府県や人口分類を選択して、グラフや表を確認してください。')
+
+    with st.expander('データの出典と注意点'):
+        st.write('・出典：政府統計の総合窓口(e-stat)「国勢調査」')
+        st.write('・大正から平成までの人口推移を確認できます')
+        st.write('・単位：人')                
 
 elif option == 'グラフ':
-    y_min = df[population_type].min()
-    y_max = df[population_type].max()
+    st.title(f'{population_type}の推移と比較')
 
-    fig = px.line(
+    # 折れ線グラフを表示
+    fig1 = px.line(
         df,
         x='西暦（年）',
         y=population_type,
         color='都道府県名',
-        range_x=[1910, df['西暦（年）'].max()],
-        range_y=[y_min, y_max]
     )
-    st.plotly_chart(fig)
+    st.plotly_chart(fig1)
+
+    # 最新年の合計を表示
+    latest_year = df['西暦（年）'].max()
+    total_value = df[df['西暦（年）'] == latest_year][population_type].sum()
+    st.metric(
+        label=f'{latest_year}年の選択合計',
+        value=f'{int(total_value):,}人'
+        )
+
+    # 棒グラフを表示
+    st.write(f'{latest_year}年の比較')
+    # 最新年だけのデータをグラフにする
+    df_latest = df[df['西暦（年）'] == latest_year]
+    fig2 = px.bar(
+        df_latest,
+        x = '都道府県名',
+        y = population_type,
+        color = '都道府県名'
+    )
+    st.plotly_chart(fig2)
+
+elif option == '表':
+    st.title('データ一覧')
+    st.write(f'{selected_pre}のデータです')
+    st.dataframe(df[['都道府県名', '西暦（年）', population_type]])
